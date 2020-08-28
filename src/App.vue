32fd936f795eb29a9f8897dd13bc8a1e2b337e3a
KauanRakoski <kauan.k.rakoski@gmail.com>
2020-08-28 20:26:57 -0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello world"/>
+    <Todos v-on:del-todo="deleteTodo" v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/Todos.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Todos
+  },
+  data() {
+    return{
+      todos: [{
+        id: 1,
+        title: "Poop",
+        completed: false
+      }]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 }
 </script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-bind:class="{'is-completed': todo.completed}">
+        <input type="checkbox" v-on:change="markComplete">
+        <label>{{todo.title}}</label>
+        <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete(){
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .is-completed{
+        text-decoration: line-through;
+        color: gray;
+    }
+</style>
