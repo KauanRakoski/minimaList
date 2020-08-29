aedf50db028c24cbd84f58aa2ed40bef2333b405
KauanRakoski <kauan.k.rakoski@gmail.com>
2020-08-29 14:56:00 -0300

    Add feature

---
+++ src/components/Add.vue
@@ -0,0 +1,36 @@
+<template>
+    <form @submit="AddTodo">
+        <input type="text" v-model="title">
+        <input type="submit" value="Submit">
+    </form>
+</template>
+
+<script>
+    import {v4} from 'uuid';
+    export default {
+        name: "Add",
+        data(){
+            return {
+                title: "",
+                
+            }
+        },
+        methods: {
+            AddTodo(e){
+                e.preventDefault();
+                const newTodo = {
+                    id: v4(),
+                    title: this.title,
+                    completed: false
+                }
+                this.$emit("add-todo", newTodo);
+
+                this.title = ''
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
