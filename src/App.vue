070f643665cce9712e382d37f245756fdcb5f53a
KauanRakoski <kauan.k.rakoski@gmail.com>
2020-08-29 14:56:26 -0300

    header + add feature

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Header/>
     <HelloWorld msg="Hello world"/>
+    <Add v-on:add-todo="addTodo" />
     <Todos v-on:del-todo="deleteTodo" v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Todos from './components/Todos.vue'
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Todos
-  },
-  data() {
-    return{
-      todos: [{
-        id: 1,
-        title: "Poop",
-        completed: false
-      }]
+  import HelloWorld from './components/HelloWorld.vue';
+  import Todos from './components/Todos.vue';
+  import Add from './components/Add.vue';
+  import Header from './components/layout/Header'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      Todos,
+      Add,
+      Header
+    },
+    data() {
+      return{
+        todos: []
+      }
+    },
+    methods: {
+      deleteTodo(id){
+        this.todos = this.todos.filter(todo => todo.id !== id);
+      },
+      addTodo(newTodo){
+        this.todos = [...this.todos, newTodo]
+      }
     }
-  },
-  methods: {
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    }
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
